0f544936c00804235cb8567e4b1bc6891f00ee49
zjx <2252474825@qq.com>
2023-02-18 18:33:48 +0800

    feat: 更新ast

--- ast.ts
+++ ast.ts
@@ -1,67 +1,67 @@
-import { Token, TokenTypes } from "./tokenizer";
 export enum NodeTypes {
-    Root,
-    Number,
-    CallExpression
+    NumberLiteral = "NumberLiteral",
+    Program = "Program",
+    StringLiteral = "StringLiteral",
+    CallExpression = "CallExpression",
 }
-interface Node {
-    type: NodeTypes
+
+export type ChildNode =
+    | NumberLiteralNode
+    | CallExpressionNode
+    | StringLiteralNode;
+
+export interface Node {
+    type: NodeTypes;
 }
-// 这里就是在后面比如params里面放的不一定是number，可能是表达式，所以我们可以将其抽离出来。
-export type ChildNode = NumberNode | CallExpressionNode
+
+export interface NumberLiteralNode extends Node {
+    type: NodeTypes.NumberLiteral;
+    value: string;
+}
+
+export interface StringLiteralNode extends Node {
+    value: string;
+    type: NodeTypes.StringLiteral;
+}
+
+export interface CallExpressionNode extends Node {
+    name: string;
+    params: ChildNode[];
+    type: NodeTypes.CallExpression;
+    context?: ChildNode[];
+}
+
 export interface RootNode extends Node {
-    body: ChildNode[]
+    body: ChildNode[];
+    type: NodeTypes.Program;
+    context?: ChildNode[];
 }
-interface NumberNode extends Node {
-    value: string
-}
-interface CallExpressionNode extends Node {
-    name: string,
-    params: ChildNode[]
-}
-function createRootNode(): RootNode {
+
+export function createStringLiteralNode(value): StringLiteralNode {
     return {
-        type: NodeTypes.Root,
-        body: []
-    }
+        type: NodeTypes.StringLiteral,
+        value,
+    };
 }
-function createNumberNode(value: string): NumberNode {
+
+export function createRootNode(): RootNode {
     return {
-        type: NodeTypes.Number,
-        value
-    }
+        type: NodeTypes.Program,
+        body: [],
+    };
 }
-function createCallExpressionNode(name: string): CallExpressionNode {
+
+export function createNumberLiteralNode(value: string): NumberLiteralNode {
+    return {
+        type: NodeTypes.NumberLiteral,
+        value,
+    };
+}
+
+export function createCallExpression(name): CallExpressionNode {
     return {
         type: NodeTypes.CallExpression,
         name,
-        params: []
-    }
-}
-function parser(tokens: Token[]) {
-    let current = 0
-    let token = tokens[current];
-    const rootNode = createRootNode()
-    while (current < tokens.length) {
-        if (token.type === TokenTypes.Number) {
-            const numberNode = createNumberNode(token.value)
-            rootNode.body.push(numberNode)
-        }
-        if (token.type === TokenTypes.Paren && token.value === "(") {
-            token = tokens[++current]
-            const node = createCallExpressionNode(token.value)
-            token = tokens[++current]
-            while (!(token.type === TokenTypes.Paren && token.value === ")")) {
-                if (token.type === TokenTypes.Number) {
-                    const numberNode = createNumberNode(token.value)
-                    node.params.push(numberNode)
-                    token = tokens[++current]
-                }
-            }
-            current++
-            rootNode.body.push(node)
-        }
-    }
-
-    return rootNode
+        params: [],
+    };
 }
